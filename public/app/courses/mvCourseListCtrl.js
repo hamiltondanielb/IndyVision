c27964b7e30b1d47d3c883bddc09ef1d5f7e1b3f
Dan Hamilton <daniel.hamilton@inin.com>
2014-05-22 13:57:04 -0400

    cached data

--- public/app/courses/mvCourseListCtrl.js
+++ public/app/courses/mvCourseListCtrl.js
@@ -1,5 +1,5 @@
-angular.module('app').controller('mvCourseListCtrl',function($scope, mvCourse){
-    $scope.courses = mvCourse.query();
+angular.module('app').controller('mvCourseListCtrl',function($scope, mvCachedCourses){
+    $scope.courses = mvCachedCourses.query();
 
     $scope.sortOptions = [{value:"title",text:"Sort by Title"},
         {value:"published",text:"Sort by Publish Date"}];
--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -1,3 +1,3 @@
-angular.module('app').controller('mvMainCtrl', function($scope,mvCourse) {
-    $scope.courses = mvCourse.query();
+angular.module('app').controller('mvMainCtrl', function($scope,mvCachedCourses) {
+    $scope.courses = mvCachedCourses.query();
 });
