9cd0b572189e777784e28097d6030f77f4b0805c
Dan Hamilton <daniel.hamilton@inin.com>
2014-05-30 14:00:32 -0400

    added google maps

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngResource', 'ngRoute']);
+angular.module('app', ['ngResource', 'ngRoute','google-maps']);
 
 angular.module('app').config(function($routeProvider, $locationProvider) {
   var routeRoleChecks = {
--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -1,3 +1,10 @@
 angular.module('app').controller('mvMainCtrl', function($scope,mvCachedEvents) {
+    $scope.map = {
+        center: {
+            latitude: 39.7910,
+            longitude: -86.1480
+        },
+        zoom: 8
+    };
     $scope.events = mvCachedEvents.query();
 });
